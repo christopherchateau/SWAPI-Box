00fbc88fcd7958f0c0913cfa151413d51d2ad11b
Drake Bott <bott.da@gmail.com>
2018-10-08 15:43:24 -0600

    Add API call for Episode data and pass to SideScroll

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,10 +1,37 @@
 import React, { Component } from 'react';
+import SideScroll from '../SideScroll';
 import './App.css';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      episodeData: {}
+    };
+  }
+
+  componentDidMount() {
+    this.getEpisodeData();
+  }
+
+  getEpisodeData() {
+    const randomEpisodeNumber = Math.floor(Math.random() * 7) + 1;
+    fetch(`https://swapi.co/api/films/${randomEpisodeNumber}`)
+      .then(response => response.json())
+      .then(json => {
+        return {
+          opening_crawl: json.opening_crawl,
+          title: json.title,
+          release_date: json.release_date
+        }})
+      .then(episodeData => this.setState({episodeData}))
+      .catch(err => console.log('Error fetching episode data'));
+  }
   render() {
+    const {episodeData} = this.state;
     return (
-      <div className="App"><h1>asdf</h1>
+      <div className="App">
+        <SideScroll episodeData={episodeData}/>
       </div>
     );
   }

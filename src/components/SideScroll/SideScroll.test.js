1177078d06897364aa32d76664ebc92797f53f0f
Drake Bott <bott.da@gmail.com>
2018-10-08 18:30:38 -0600

    Add snapshot test for SideScroll component

--- src/components/SideScroll/SideScroll.test.js
+++ src/components/SideScroll/SideScroll.test.js
@@ -4,13 +4,18 @@ import { shallow } from "enzyme";
 
 describe("SideScroll", () => {
   let wrapper;
+  const exampleData = {
+    title: 'A New Hope',
+    opening_crawl: 'Opening crawl text here',
+    release_date: '1977-05-25'
+  };
 
   beforeEach(() => {
-    wrapper = shallow(<SideScroll />);
+    wrapper = shallow(<SideScroll episodeData={exampleData}/>);
   });
 
-  it("", () => {
-
+  it("Should match snapshot", () => {
+    expect(wrapper).toMatchSnapshot();
   });
-  
+
 });

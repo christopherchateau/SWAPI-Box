cd08f2566d81b391862cf55db3646e84acc58da4
Drake Bott <bott.da@gmail.com>
2018-10-09 12:08:37 -0600

    Add get people functionality

--- src/helper/helper.js
+++ src/helper/helper.js
@@ -1,18 +1,38 @@
 class SWapi {
-  constructor() {
-  }
-
   getRandomEpisode() {
     const randomEpisodeNumber = Math.floor(Math.random() * 7) + 1;
-    return fetch(`https://swapi.co/api/films/${randomEpisodeNumber}`)
-      .then(response => response.json())
-      .then(json => {
+    return this.getData(`films/${randomEpisodeNumber}`)
+      .then(episode => {
         return {
-          opening_crawl: json.opening_crawl,
-          title: json.title,
-          release_date: json.release_date
-        }})
-      .catch(err => console.log('Error fetching episode data'))
+          opening_crawl: episode.opening_crawl,
+          title: episode.title,
+          release_date: episode.release_date
+        }});
+  }
+  async getPeople() {
+    if (!this.species) {
+      await this.getSpecies();
+    }
+    const response = await this.getData('people')
+    const people = response.results.map(person => {
+      const speciesData = person.species[0]
+      const speciesID = speciesData.charAt(speciesData.length - 2);
+      return {
+        name: person.name,
+        homeworld: person.homeworld,
+        species: this.species[speciesID].name,
+      }
+      })
+    console.log(people);
+  }
+  getSpecies() {
+    this.getData('species')
+      .then(response => this.species = response.results);
+  }
+  getData(type) {
+    return fetch(`https://swapi.co/api/${type}`)
+      .then(response => response.json())
+      .catch(err => console.log(`Error fetching ${type} data`));
   }
 }
 

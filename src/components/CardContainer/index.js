d5fd5a38b3f00fdbe89bec3ef5da6441d1e159e9
Chris Chateau <christopherchateau@gmail.com>
2018-10-08 20:54:01 -0600

    Add styling to card container

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -4,7 +4,7 @@ import './CardContainer.css';
 const CardContainer = (props) => {
   return (
     <div className='card-container'>
-      <h2>This is a Card Container</h2>
+      <h2></h2>
     </div>
   );
 }

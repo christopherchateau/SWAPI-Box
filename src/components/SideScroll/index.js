1646cc872ed474def1eeed12840e302c21b52b49
Drake Bott <bott.da@gmail.com>
2018-10-08 18:16:57 -0600

    Add scrolling text animation

--- src/components/SideScroll/index.js
+++ src/components/SideScroll/index.js
@@ -5,9 +5,11 @@ const SideScroll = (props) => {
   const {title, opening_crawl, release_date} = props.episodeData;
   return (
     <div className='SideScroll'>
-      <p>{opening_crawl}</p>
-      <h2>{title}</h2>
-      <h3>{release_date}</h3>
+      <div className='scroll'>
+        <p className='scrollText'>{opening_crawl}</p>
+        <p className='scrollText'>{title}</p>
+        <p className='scrollText'>{release_date}</p>
+      </div>
     </div>
   );
 };

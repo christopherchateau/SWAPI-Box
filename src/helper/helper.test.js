660b2e3316fca2f703da047d3e600ea566a41b54
Drake Bott <bott.da@gmail.com>
2018-10-10 15:20:36 -0600

    Test getData function, rewrite tests to use async await/ jest .resolved

--- src/helper/helper.test.js
+++ src/helper/helper.test.js
@@ -1,28 +1,56 @@
-import SWapi from './helper'
+import * as API from './helper'
 
 describe('SWAPI', () => {
-  let api;
-  beforeEach(() => {
-    api = new SWapi();
-    window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
-      json: () => Promise.resolve({
+  describe('getData', () => {
+    let extension;
+    beforeEach(() => {
+      extension = 'films/1';
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: () => ({data: 'here\'s some stuff'})
+      }));
+    });
+
+    it('Should call fetch with the argument as a path', () => {
+      const expected = 'https://swapi.co/api/' + extension;
+      API.getData(extension);
+      expect(window.fetch).toHaveBeenCalledWith(expected);
+    });
+
+    it('Should return an object from the fetch call', () => {
+      const expected = {data: 'here\'s some stuff'};
+      expect(API.getData(extension)).resolves.toEqual(expected);
+    });
+
+    it('Should return an error when fetch does not work', () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.reject(
+        Error('failed')))
+      expect(API.getData(extension)).resolves.toBe('failed');
+    });
+  });
+
+  describe('getRandomEpisode', () => {
+
+    it('Should fetch a random movie\'s data', async () => {
+      window.fetch = jest.fn().mockImplementation(() => Promise.resolve({
+        json: () => ({
+          title: 'A New Hope',
+          opening_crawl: 'Sample Text',
+          release_date: 'Date'
+        })
+      }));
+      const expected = {
         title: 'A New Hope',
-        opening_crawl: 'Sample Text'
-      })
-    }))
+        opening_crawl: 'Sample Text',
+        release_date: 'Date'
+      }
+      const episode =  await API.getRandomEpisode()
+      expect(episode).toEqual(expected);
+    });
   });
 
-  it('Should match snapshot', () => {
-    expect(api).toMatchSnapshot();
-  });
-
-  it('Should fetch a random movie\'s data', () => {
-    Promise.resolve(api.getRandomEpisode())
-      .then((episode) => expect(episode).toBeDefined())
-      .catch((err) => console.log('Test 2 error!'));
-  });
-
-  it('Should fetch species and people', () => {
-    api.getPeople();
+  describe('getPeople', () => {
+    it('Should fetch species and people', () => {
+      API.getPeople();
+    });
   });
 });

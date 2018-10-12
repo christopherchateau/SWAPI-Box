6550b2fe1907881a29f42c31d8697302f2f9e534
Chris Chateau <christopherchateau@gmail.com>
2018-10-11 20:08:22 -0600

    Add yoda pic, tweak card contianer image styling

--- src/components/CardContainer/index.js
+++ src/components/CardContainer/index.js
@@ -2,6 +2,7 @@ import React, { PureComponent } from "react";
 import Card from "../Card";
 import PropTypes from "prop-types";
 import obiWan from "../../images/obi-wan.jpg";
+import yoda from "../../images/yoda.png";
 import "./CardContainer.css";
 
 class CardContainer extends PureComponent {
@@ -12,11 +13,16 @@ class CardContainer extends PureComponent {
 
   render() {
     const { cardData, handleCardClick, selectedCategory } = this.props;
-    let displayText;
-    selectedCategory === "initial"
-      ? (displayText = "$ select a category $")
-      : (displayText = "These are not the cards you are looking for!");
-      
+    let displayText, characterPic;
+    if (selectedCategory === "initial") {
+      displayText = "$ select a category, you must $";
+      characterPic = yoda;
+    } else {
+      displayText = "These are not the cards you are looking for!";
+      characterPic = obiWan;
+    }
+
+    console.log(selectedCategory);
     const cards = cardData.map((card, index) => {
       return (
         <Card
@@ -33,7 +39,7 @@ class CardContainer extends PureComponent {
       return (
         <div className="CardContainer center">
           <h1 className="emptyContainerMessage">{displayText}</h1>
-          <img className="obiWanPic" src={obiWan} />
+          <img className="obiWanPic" src={characterPic} />
         </div>
       );
     }

c88731b85a5216e8b5a3644513664f8dc9c09f03
Drake Bott <bott.da@gmail.com>
2018-10-14 11:02:40 -0600

    Write CardContainer tests

--- src/components/CardContainer/CardContainer.test.js
+++ src/components/CardContainer/CardContainer.test.js
@@ -3,19 +3,68 @@ import { shallow, mount } from "enzyme";
 import CardContainer from "./index";
 
 describe("CardContainer", () => {
-  let wrapper;
-
-  beforeEach(() => {
-    wrapper = shallow(
+  it("Renders like snapshot", () => {
+    const wrapper = shallow(
       <CardContainer
         cardData={[]}
         handleCardClick={jest.fn()}
         selectedCategory={"planets"}
       />
     );
+    expect(wrapper).toMatchSnapshot();
+  });
+  it("Should render message stating no category has been selected", () => {
+    const wrapper = shallow(
+    <CardContainer
+      cardData={[]}
+      handleCardClick={jest.fn()}
+      selectedCategory={"initial"}
+    />
+  );
+    const message = wrapper.find(".emptyContainerMessage");
+    expect(message.text()).toBe("$ select a category, you must $");
+  });
+  it("Should render message stating there are no given favorites", () => {
+    const wrapper = shallow(
+    <CardContainer
+      cardData={[]}
+      handleCardClick={jest.fn()}
+      selectedCategory={"planets"}
+    />
+  );
+    const message = wrapper.find(".emptyContainerMessage");
+    expect(message.text()).toBe("These are not the cards you are looking for!");
   });
 
-  it("Renders like snapshot", () => {
-    expect(wrapper).toMatchSnapshot();
+  it("Should not render a message when cards are given", () => {
+    const wrapper = shallow(
+    <CardContainer
+      cardData={[{
+        name: 'Luke Skywalker',
+        species: 'Human',
+        language: 'Galactic Basic',
+        population: 200000}]}
+      handleCardClick={jest.fn()}
+      selectedCategory={"planets"}
+    />
+    );
+    const message = wrapper.find(".emptyContainerMessage");
+    expect(message.length).toBe(0);
+  });
+
+  it("Should render cards when data is given", () => {
+    const wrapper = shallow(
+    <CardContainer
+      cardData={[{
+        name: 'Luke Skywalker',
+        species: 'Human',
+        language: 'Galactic Basic',
+        population: 200000}]}
+      handleCardClick={jest.fn()}
+      selectedCategory={"planets"}
+    />
+    );
+    const message = wrapper.find("Card");
+    expect(message.length).toBe(1);
   });
 });

75b297425ece9f254f1ac81eef33b2083aeb4b1b
Drake Bott <bott.da@gmail.com>
2018-10-15 14:33:41 -0600

    Write toggleFavorites tests

--- src/components/App/App.test.js
+++ src/components/App/App.test.js
@@ -3,65 +3,98 @@ import { shallow, mount } from "enzyme";
 import App from "./index";
 
 describe("App", () => {
-  let wrapper;
-  let mockPeople;
-  let mockPlanets;
+  describe("Rendering", () => {
+    let wrapper;
+    let mockPeople;
+    let mockPlanets;
 
-  beforeEach(() => {
-    wrapper = shallow(<App />);
-    mockPeople = [
-      {
-        name: "C-3PO",
-        homeworld: "Tatooine",
-        language: "n/a",
-        population: "200000",
-        species: "Droid",
-        favorited: false
-      },
-      {
-        name: "Luke Skywalker",
-        homeworld: "Tatooine",
-        language: "Galactic Basic",
-        population: "200000",
-        species: "Human",
-        favorited: false
-      }
-    ];
-    mockPlanets = [
-      {
-        name: "Alderaan",
-        climate: "temperate",
-        population: "2000000000",
-        residents: [" Leia Organa", " Bail Prestor Organa", " Raymus Antilles"],
-        terrain: "grasslands, mountains",
-        favorited: false
-      },
-      {
-        name: "Yavin IV",
-        climate: "temperate, tropical",
-        population: "1000",
-        residents: [],
-        terrain: "jungle, rainforests",
-        favorited: true
-      }
-    ];
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+      mockPeople = [
+        {
+          name: "C-3PO",
+          homeworld: "Tatooine",
+          language: "n/a",
+          population: "200000",
+          species: "Droid",
+          favorited: false
+        },
+        {
+          name: "Luke Skywalker",
+          homeworld: "Tatooine",
+          language: "Galactic Basic",
+          population: "200000",
+          species: "Human",
+          favorited: false
+        }
+      ];
+      mockPlanets = [
+        {
+          name: "Alderaan",
+          climate: "temperate",
+          population: "2000000000",
+          residents: [" Leia Organa", " Bail Prestor Organa", " Raymus Antilles"],
+          terrain: "grasslands, mountains",
+          favorited: false
+        },
+        {
+          name: "Yavin IV",
+          climate: "temperate, tropical",
+          population: "1000",
+          residents: [],
+          terrain: "jungle, rainforests",
+          favorited: true
+        }
+      ];
+    });
+
+    it("selected property in state should initialize as 'initial' ", () => {
+      expect(wrapper.state().selected).toEqual("initial");
+    });
+
+    it("updateData should add data to appropriate array in state", () => {
+      wrapper.instance().updateData("people", mockPeople);
+      expect(wrapper.state().people).toHaveLength(2);
+      expect(wrapper.state().people[1].name).toEqual("Luke Skywalker");
+    });
   });
 
-  it("selected property in state should initialize as 'initial' ", () => {
-    expect(wrapper.state().selected).toEqual("initial");
+  describe("handleCardClick", () => {
+    let wrapper;
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+    });
+
+    it("Should add a favorited card to the given category in state", () => {
+      wrapper.state().selected = "people";
+      const sampleCard = { name: "Luke Skywalker", homeworld: "Tatooine"};
+      wrapper.instance().handleCardClick(sampleCard, false);
+
+      expect(wrapper.state().favorites.people).toEqual([sampleCard]);
+    });
+
+    it("Should remove a card that is unfavorited in state", () => {
+      wrapper.state().selected = "people";
+      const sampleCard = { name: "Luke Skywalker", homeworld: "Tatooine"};
+      wrapper.instance().handleCardClick(sampleCard, false);
+      wrapper.instance().handleCardClick(sampleCard, true);
+      expect(wrapper.state().favorites.people.length).toBe(0);
+    });
   });
 
-  it("updateData should add data to appropriate array in state", () => {
-    wrapper.instance().updateData("people", mockPeople);
-    expect(wrapper.state().people).toHaveLength(2);
-    expect(wrapper.state().people[1].name).toEqual("Luke Skywalker");
-  });
+  describe("toggleFavorites", () => {
+    let wrapper;
 
-  it("toggleFavorites should toggle", () => {
-    wrapper.instance().updateData("planets", mockPlanets);
-    console.log(wrapper.state());
-    wrapper.instance().toggleFavorites();
-    console.log(wrapper.state());
-    
+    beforeEach(() => {
+      wrapper = shallow(<App />);
+    });
+
+    it("Should change selected state data out for favorites data", () => {
+      const expected = ["Luke", "Leia", "Vader"];
+      wrapper.state().favorites.people = expected;
+      wrapper.state().selected = "people";
+      wrapper.instance().toggleFavorites();
+      expect(wrapper.state().people).toEqual(expected);
+    });
   });
 });

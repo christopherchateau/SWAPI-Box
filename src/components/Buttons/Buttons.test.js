3e9d7dd25dc41a66821b82206ecef075f62bd98b
Drake Bott <bott.da@gmail.com>
2018-10-14 18:43:00 -0600

    Add test for conditional selected classnames

--- src/components/Buttons/Buttons.test.js
+++ src/components/Buttons/Buttons.test.js
@@ -3,15 +3,37 @@ import Buttons from "./index";
 import { shallow } from "enzyme";
 
 describe("Buttons", () => {
-  let wrapper;
+  let wrapper, updateData;
 
   beforeEach(() => {
+    updateData = jest.fn()
     wrapper = shallow(
-      <Buttons updateData={jest.fn()} selectedCategory={"planets"} />
+      <Buttons updateData={updateData} selectedCategory={"planets"} />
     );
   });
 
   it("Renders like snapshot", () => {
     expect(wrapper).toMatchSnapshot();
+  });
+
+  it("Should add selected className to planets when selected", () => {
+    const planets = wrapper.find(".planets");
+    expect(planets.hasClass("selected")).toBe(true);
+  });
+
+  it("Should add selected className to people when selected", () => {
+    wrapper = shallow(
+      <Buttons updateData={updateData} selectedCategory={"people"} />
+    );
+    const people = wrapper.find(".people");
+    expect(people.hasClass("selected")).toBe(true);
+  });
+
+  it("Should add selected className to vehicles when selected", () => {
+    wrapper = shallow(
+      <Buttons updateData={updateData} selectedCategory={"vehicles"} />
+    );
+    const vehicles = wrapper.find(".vehicles");
+    expect(vehicles.hasClass("selected")).toBe(true);
   });
 });

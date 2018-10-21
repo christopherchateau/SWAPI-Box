bc5baceb9f8e912092ce89a56847e2066170b6a5
Drake Bott <bott.da@gmail.com>
2018-10-20 20:46:11 -0600

    Write code to pass localStorage tests

--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -14,23 +14,62 @@ export const getRandomEpisode = () => {
 };
 
 export const getPeople = () => {
+  if (localStorage.getItem("people")) {
+    const people = localStorage.getItem("people");
+    return JSON.parse(people);
+  }
   return fetch(url + "people")
     .then(response => response.json())
-    .then(people => people.results);
+    .then(people => people.results)
+    .then(people => {
+      const stringifyPeople = JSON.stringify(people);
+      localStorage.setItem("people", stringifyPeople);
+      return people;
+    });
 };
 
 export const getPlanets = () => {
+  if (localStorage.getItem("planets")) {
+    const planets = localStorage.getItem("planets");
+    return JSON.parse(planets);
+  }
+
   return fetch(url + "planets")
     .then(response => response.json())
-    .then(planets => planets.results);
+    .then(planets => planets.results)
+    .then(planets => {
+      const stringifyPlanets = JSON.stringify(planets);
+      localStorage.setItem("planets", stringifyPlanets);
+      return planets;
+    });
 };
 
 export const getVehicles = () => {
+  if (localStorage.getItem("vehicles")) {
+    const vehicles = localStorage.getItem("vehicles");
+    return JSON.parse(vehicles);
+  }
+
   return fetch(url + "vehicles")
     .then(response => response.json())
-    .then(vehicles => vehicles.results);
+    .then(vehicles => vehicles.results)
+    .then(vehicles => {
+      const stringifyVehicles = JSON.stringify(vehicles);
+      localStorage.setItem("vehicles", stringifyVehicles);
+      return vehicles;
+    });
 };
 
 export const getEndpoint = url => {
-  return fetch(url).then(response => response.json());
+  if (localStorage.getItem(url)) {
+    const endpoint = localStorage.getItem(url);
+    return JSON.parse(endpoint);
+  }
+
+ return fetch(url).then(response => response.json())
+    .then(endpoint => {
+      const stringifyData = JSON.stringify(endpoint);
+      localStorage.setItem(url, stringifyData);
+      return endpoint;
+    });
 };

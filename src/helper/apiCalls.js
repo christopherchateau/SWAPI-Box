d8bf935d46b182172bd35a3a817e192fe7055b18
Chris Chateau <christopherchateau@gmail.com>
2018-10-21 13:14:39 -0600

    Update package.json for github pages deployment

--- src/helper/apiCalls.js
+++ src/helper/apiCalls.js
@@ -7,51 +7,53 @@ export const getRandomEpisode = async () => {
   }
   const response = await fetch(url + `films/${randomEpisodeNumber}`);
   const episode = await response.json();
-  localStorage.setItem(`episode${randomEpisodeNumber}`, JSON.stringify(episode));
+  localStorage.setItem(
+    `episode${randomEpisodeNumber}`,
+    JSON.stringify(episode)
+  );
   return episode;
 };
 
 export const getPeople = async () => {
-  if (localStorage.getItem("people")) {
-    const people = localStorage.getItem("people");
-    return JSON.parse(people);
-  }
+  // if (localStorage.getItem("people")) {
+  //   const people = localStorage.getItem("people");
+  //   return JSON.parse(people);
+  // }
   const response = await fetch(url + "people");
   const people = await response.json();
-  localStorage.setItem("people", JSON.stringify(people.results));
+  //localStorage.setItem("people", JSON.stringify(people.results));
   return people.results;
-
 };
 
 export const getPlanets = async () => {
-  if (localStorage.getItem("planets")) {
-    const planets = localStorage.getItem("planets");
-    return JSON.parse(planets);
-  }
+  // if (localStorage.getItem("planets")) {
+  //   const planets = localStorage.getItem("planets");
+  //   return JSON.parse(planets);
+  // }
   const response = await fetch(url + "planets");
   const planets = await response.json();
-  localStorage.setItem("planets", JSON.stringify(planets.results));
+  //localStorage.setItem("planets", JSON.stringify(planets.results));
   return planets.results;
 };
 
 export const getVehicles = async () => {
-  if (localStorage.getItem("vehicles")) {
-    const vehicles = localStorage.getItem("vehicles");
-    return JSON.parse(vehicles);
-  }
+  // if (localStorage.getItem("vehicles")) {
+  //   const vehicles = localStorage.getItem("vehicles");
+  //   return JSON.parse(vehicles);
+  // }
   const response = await fetch(url + "vehicles");
   const vehicles = await response.json();
-  localStorage.setItem("vehicles", JSON.stringify(vehicles.results));
+  //localStorage.setItem("vehicles", JSON.stringify(vehicles.results));
   return vehicles.results;
 };
 
 export const getEndpoint = async url => {
-  if (localStorage.getItem(url)) {
-    const endpoint = localStorage.getItem(url);
-    return JSON.parse(endpoint);
-  }
+  // if (localStorage.getItem(url)) {
+  //   const endpoint = localStorage.getItem(url);
+  //   return JSON.parse(endpoint);
+  // }
   const response = await fetch(url);
   const endpoint = await response.json();
-  localStorage.setItem(url, JSON.stringify(endpoint));
+  //localStorage.setItem(url, JSON.stringify(endpoint));
   return endpoint;
 };

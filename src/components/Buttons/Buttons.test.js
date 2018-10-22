c86bafabc309e7d524ec90ebc36dcfe77e0991ac
Chris Chateau <christopherchateau@gmail.com>
2018-10-22 07:53:51 -0600

    Remove button tests

--- src/components/Buttons/Buttons.test.js
+++ src/components/Buttons/Buttons.test.js
@@ -12,36 +12,5 @@ describe("Buttons", () => {
       );
       expect(wrapper).toMatchSnapshot();
     });
-    
-    describe("onClick", () => {
-      let wrapper;
-      let API = {};
-
-      beforeEach(() => {
-        wrapper = shallow(
-          <Buttons updateData={jest.fn()} selectedCategory={"planets"} />
-        );
-      });
-
-      it.skip("Should call getPeople when people button is clicked", () => {
-        const peopleBtn = wrapper.find(".people");
-        peopleBtn.simulate("click");
-        expect(wrapper.instance().updateData("planets")).toHaveBeenCalled();
-      });
-
-      it.skip("Should call getPlanets when planets button is clicked", () => {
-        let planets = jest.fn();
-        const planetBtn = wrapper.find(".planets");
-        planetBtn.simulate("click");
-        expect(planets).toHaveBeenCalled();
-      });
-
-      it.skip("Should call getVehicles when vehicles button is clicked", () => {
-        API.getVehicles = jest.fn();
-        const vehicleBtn = wrapper.find(".vehicles");
-        vehicleBtn.simulate("click");
-        expect(API.getVehicles).toHaveBeenCalled();
-      });
-    });
   });
 });

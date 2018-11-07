60390a8ddaa9a4fb930bac90acd4dcaa875ac3f2
Chris Chateau <christopherchateau@gmail.com>
2018-11-06 17:40:22 -0700

    Updates

---
+++ precache-manifest.fef7c232bb8f2a2042e095b885ac0906.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "a312f81ecbf659e8662994cbb96a52a4",
+    "url": "/SWAPI-Box/static/media/stars-bg.a312f81e.jpg"
+  },
+  {
+    "revision": "fbf57569107072436ddcd5ef61568f11",
+    "url": "/SWAPI-Box/static/media/obi-wan.fbf57569.jpg"
+  },
+  {
+    "revision": "cc3019aa6439ef893839b8a0d6d0391d",
+    "url": "/SWAPI-Box/static/media/Starjedi.cc3019aa.ttf"
+  },
+  {
+    "revision": "92940b4e8c42006a4786",
+    "url": "/SWAPI-Box/static/js/runtime~main.92940b4e.js"
+  },
+  {
+    "revision": "25dd8552704f40722133",
+    "url": "/SWAPI-Box/static/js/main.25dd8552.chunk.js"
+  },
+  {
+    "revision": "3e2b7080390cfff05d0f",
+    "url": "/SWAPI-Box/static/js/1.3e2b7080.chunk.js"
+  },
+  {
+    "revision": "25dd8552704f40722133",
+    "url": "/SWAPI-Box/static/css/main.8d75e7b6.chunk.css"
+  },
+  {
+    "revision": "200f99f73a3cd510e3e017bdb9f708e6",
+    "url": "/SWAPI-Box/index.html"
+  }
+];
